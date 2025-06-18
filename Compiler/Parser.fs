b2d46b22fe0cdad8aed6468eb11afb7a1341c407
Arthur Baroi <arthurcbaroi@hotmail.com>
2025-06-18 15:05:02 -0500

    Added initial code (not buildable).

--- Compiler/Parser.fs
+++ Compiler/Parser.fs
@@ -1,2 +1,17 @@
-﻿module Parser
+﻿namespace Compiler
+
+open System
+open System.Collections.Generic
+
+open Tokenizer
+
+// This module is responsible for parsing tokens into a structured representation.
+module Parser =
+    type ParseError = string
+    type Parser<'T> = TokenType list -> Result<'T, ParseError>
+    let parse (parser: Parser<'T>) (tokens: TokenType list) : Result<'T, ParseError> =
+        parser tokens
+    let runParser (parser: Parser<'T>) (input: string) : Result<'T, ParseError> =
+        let tokens = Tokenizer.tokenize input
+        parse parser tokens
 
--- Compiler/Tokenizer.fs
+++ Compiler/Tokenizer.fs
@@ -1,5 +1,15 @@
 ﻿namespace Compiler
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+
+// This module is responsible for tokenizing input strings into a sequence of tokens.
+module Tokenizer =
+
+    type TokenType =
+        | Identifier of string
+        | Keyword of string
+        | Number of int
+        | String of string
+        | Operator of string
+        | Punctuation of string
+        | Whitespace
